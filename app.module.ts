e73027d86eb32ee478fada29866a8cc67b411c50
Leilani97 <55418872+Leilani97@users.noreply.github.com>
2019-10-20 22:46:23 -0500

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+// Components
+import { AlertComponent } from './_components';
+import { ProfileComponent } from './_components';
+import { LoginComponent } from './_components';
+import { RegisterComponent } from './_components';
+import { HomeComponent } from './_components';
+
+// Services
+import { AuthenticationService } from './_services';
+import { AuthGuardService } from './_services';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate:[AuthGuardService]},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuardService] }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlertComponent,
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+  ],
+  providers: [
+    AuthenticationService,
+    AuthGuardService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
