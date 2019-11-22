6d58f5cd61768ae9250e0d8e6f13553151d12981
Leilani97 <55418872+Leilani97@users.noreply.github.com>
2019-11-22 15:11:01 -0600

    Update index.ts

--- src/app/_components/index.ts
+++ src/app/_components/index.ts
@@ -6,8 +6,4 @@ export * from './profile.component';
 export * from './register.component';
 export * from './table.component';
 export * from './my-line-chart.component';
-<<<<<<< HEAD
 export * from './chat.component';
-=======
-export * from './chat.component';
->>>>>>> bd5640ef0cb2697c9b2c0321e907e0523df39441
